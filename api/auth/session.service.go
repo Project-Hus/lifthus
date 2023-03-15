cfcb472aa7ee3be6a5d6a04ea8d32394cf8bb4bb
lifthus <lifthus531@gmail.com>
2023-03-16 02:46:04 +0900

    refactored /session/new

--- api/auth/session.service.go
+++ api/auth/session.service.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 	"lifthus-auth/common"
 	"lifthus-auth/db"
+	"lifthus-auth/helper"
 	"lifthus-auth/service/session"
 	"log"
 	"net/http"
 	"os"
-	"time"
 
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 )
 
@@ -27,26 +26,19 @@ import (
 func (ac authApiController) NewSessionHandler(c echo.Context) error {
 	// get lifthus_st from cookie
 	lifthus_st, err := c.Cookie("lifthus_st")
-	if err != nil {
-		if err != http.ErrNoCookie {
-			log.Println("[F] getting lifthus_st from cookie failed: ", err)
-			return c.NoContent(http.StatusInternalServerError)
-		}
+	if err != nil && err != http.ErrNoCookie {
+		log.Println("[F] getting lifthus_st from cookie failed: ", err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 	// if there is session cookie already, just maintain the session.
 	// it makes the session to be maintained through the whole tabs of the browser unlike using session storage.
 	if lifthus_st != nil {
 		// parse lifthus_st
-		st, err := jwt.Parse(lifthus_st.Value, func(token *jwt.Token) (interface{}, error) {
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-			}
-			return []byte(os.Getenv("HUS_SECRET_KEY")), nil
-		})
-		if err != nil || !st.Valid {
+		st, exp, err := helper.ParseJWTwithHMAC(lifthus_st.Value)
+		if err != nil || !exp {
 			return c.NoContent(http.StatusInternalServerError)
 		}
-		sid := st.Claims.(jwt.MapClaims)["sid"].(string)
+		sid := st["sid"].(string)
 		return c.String(http.StatusOK, sid)
 	}
 	/*
@@ -54,26 +46,9 @@ func (ac authApiController) NewSessionHandler(c echo.Context) error {
 		the client should clear the cookie self.
 	*/
 
-	// and if there is no cookie, create new session and return session id
-
-	// create new session and get the session id
-	sid, err := session.CreateSession(c.Request().Context(), ac.Client)
+	// and if there is no cookie, create new session and return session id.
+	sid, stSigned, err := session.CreateSession(c.Request().Context(), ac.Client)
 	if err != nil {
-		return c.NoContent(http.StatusInternalServerError)
-	}
-
-	// create new jwt session token with session id
-	st := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"sid": sid,
-		"uid": nil, // it will be omitted actually.
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
-	})
-
-	// Sign and get the complete encoded token as a string using the secret
-	hsk := []byte(os.Getenv("HUS_SECRET_KEY"))
-	stSigned, err := st.SignedString(hsk)
-	if err != nil {
-		log.Println("[F] signing session token failed: ", err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
