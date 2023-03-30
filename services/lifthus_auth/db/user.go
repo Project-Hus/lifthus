aeed3479287be9011efe6609728c8ca12766117d
lifthus <lifthus531@gmail.com>
2023-03-30 13:07:36 +0900

    lifthus migrating to SAM

---
+++ services/lifthus_auth/db/user.go
@@ -0,0 +1,50 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"lifthus-auth/common"
+	"lifthus-auth/ent"
+	"lifthus-auth/ent/user"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func CreateNewLifthusUser(c context.Context, client *ent.Client, nu common.HusSessionCheckBody) (*ent.User, error) {
+	uid_uuid, err := uuid.Parse(nu.Uid)
+	if err != nil {
+		return nil, fmt.Errorf("!!parsing uuid failed:%w", err)
+	}
+	// create new lifthus user
+	lu := client.User.Create().SetID(uid_uuid).
+		SetEmail(nu.Email).
+		SetEmailVerified(nu.EmailVerified).
+		SetName(nu.Name).
+		SetGivenName(nu.GivenName).
+		SetFamilyName(nu.FamilyName)
+	if nu.Birthdate != "" {
+		t, err := time.Parse(time.RFC3339, nu.Birthdate)
+		if err != nil {
+			return nil, fmt.Errorf("!!parsing birthdate failed:%w", err)
+		}
+		lu.SetBirthdate(t)
+	}
+	nlu, err := lu.Save(c)
+	if err != nil {
+		return nil, fmt.Errorf("!!creating new lifthus userfailed:%w", err)
+	}
+	return nlu, nil
+}
+
+func QueryUserByUID(c context.Context, client *ent.Client, uid string) (*ent.User, error) {
+	uid_uuid, err := uuid.Parse(uid)
+	if err != nil {
+		return nil, fmt.Errorf("!!parsing uuid failed:%w", err)
+	}
+	u, err := client.User.Query().Where(user.ID(uid_uuid)).Only(context.Background())
+	if err != nil && !ent.IsNotFound(err) {
+		return nil, fmt.Errorf("!!getting user by uid failed:%w", err)
+	}
+	return u, nil
+}
---
+++ services/lifthus_auth/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"lifthus-auth/common/lifthus"
+	"lifthus-auth/db"
+	"log"
+	"net/http"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	echoSwagger "github.com/swaggo/echo-swagger"
+
+	"lifthus-auth/api/auth"
+)
+
+// @title Lifthus user server
+// @version 0.0.0
+// @description This is Project-Hus's subservice Lifthus's user management server.
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.url lifthus531@gmail.com
+// @contact.email lifthus531@gmail.com
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host lifthus.com
+// @BasePath /
+func main() {
+	// set .env
+	err := godotenv.Load() // now you can use os.Getenv("VAR_NAME")
+	if err != nil {
+		log.Fatalf("[F]loading .env file failed:%v", err)
+	}
+
+	// connecting to lifthus_user_db with ent
+	client, err := db.ConnectToLifthusAuth()
+	if err != nil {
+		log.Fatalf("[F]connecting db failed:%v", err)
+	}
+	defer client.Close()
+
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("[F]creating schema resources failed : %v", err)
+	}
+
+	// initialize Lifthus common variables
+	lifthus.InitLifthusVars(os.Getenv("GOENV"), client)
+
+	// subdomains
+	hosts := map[string]*Host{}
+
+	//  Create echo web server instance and set CORS headers
+	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		// If your Backend is deployed in AWS and using API Gateway to call through,
+		// then all these headers need to be applied in API Gateway level also.
+		AllowOrigins: lifthus.Origins,
+		AllowHeaders: []string{
+			echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization,
+		},
+		AllowCredentials: true,
+		AllowMethods: []string{
+			http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete, http.MethodOptions, http.MethodPatch,
+		},
+	}))
+
+	// authApi, which controls auth all over the services
+	userApi := auth.NewAuthApiController(client)
+	hosts["localhost:9091"] = &Host{Echo: userApi} // gonna use auth.cloudhus.com later
+
+	// get requset and process by its subdomain
+	e.Any("/*", func(c echo.Context) (err error) {
+		req, res := c.Request(), c.Response()
+		host, ok := hosts[req.Host] // if the host is not registered, it will be nil.
+		if !ok {
+			return c.NoContent(http.StatusNotFound)
+		} else {
+			host.Echo.ServeHTTP(res, req)
+		}
+		return err
+	})
+
+	// provide api docs with swagger 2.0
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	// Run the server
+	e.Logger.Fatal(e.Start(":9091"))
+}
+
+type Host struct {
+	Echo *echo.Echo
+}
