b303fa6d22b0569eae57b46fc756a43693fb1f25
lifthus <lifthus531@gmail.com>
2023-04-04 01:47:08 +0900

    DB credential env fixed

--- services/lifthus-auth/db/db.go
+++ services/lifthus-auth/db/db.go
@@ -11,13 +11,13 @@ import (
 // ConncectToHusAuth returns hus_auth_db's ent client.
 // you've got to close it with Close() in defer out of this function.
 func ConnectToLifthusAuth() (*ent.Client, error) {
-	dbHost, ok1 := os.LookupEnv("LIFTHUS_DB_HOST")
-	dbPort, ok2 := os.LookupEnv("LIFTHUS_DB_PORT")
+	dbHost, ok1 := os.LookupEnv("LIFTHUS_AUTH_DB_HOST")
+	dbPort, ok2 := os.LookupEnv("LIFTHUS_AUTH_DB_PORT")
 	dbUser, ok3 := os.LookupEnv("LIFTHUS_AUTH_DB_USER")
 	dbPassword, ok4 := os.LookupEnv("LIFTHUS_AUTH_DB_PASSWORD")
 	dbName, ok5 := os.LookupEnv("LIFTHUS_AUTH_DB_NAME")
 	if !ok1 || !ok2 || !ok3 || !ok4 || !ok5 {
-		log.Fatal("HUS_DB_HOST, HUS_DB_PORT, LIFTHUS_AUTH_DB_USER, LIFTHUS_AUTH_DB_PASSWORD, LIFTHUS_AUTH_DB_NAME is not set")
+		log.Fatal("LIFTHUS_AUTH_DB credentials are not set")
 	}
 
 	// DB connection
