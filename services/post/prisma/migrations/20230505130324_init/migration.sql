5a3a9965c170f1cefe739d76190ba12d34df11a8
lifthus <lifthus531@gmail.com>
2023-05-05 22:03:41 +0900

    [post]working on post db

---
+++ services/post/prisma/migrations/20230505130324_init/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `author` BIGINT UNSIGNED NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Comment` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `author` BIGINT UNSIGNED NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `parentId` BIGINT UNSIGNED NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Mention` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `postId` BIGINT UNSIGNED NULL,
+    `comentId` BIGINT UNSIGNED NULL,
+    `mentionor` BIGINT UNSIGNED NOT NULL,
+    `mentionee` BIGINT UNSIGNED NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_parentId_fkey` FOREIGN KEY (`parentId`) REFERENCES `Comment`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Mention` ADD CONSTRAINT `Mention_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Post`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Mention` ADD CONSTRAINT `Mention_comentId_fkey` FOREIGN KEY (`comentId`) REFERENCES `Comment`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
