1e677b8e9f4e8ea76f80d24ddd6839931b664cb2
lifthus <lifthus531@gmail.com>
2023-05-07 18:59:29 +0900

    [post]openapi controller moved to post controller

--- services/post/src/openapi/openapi.module.ts
+++ services/post/src/openapi/openapi.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { OpenapiService } from './openapi.service';
-import { OpenapiController } from './openapi.controller';
 import { AppModule } from 'src/app.module';
 
 @Module({
   imports: [AppModule],
-  //controllers: [OpenapiController],
-  //providers: [OpenapiService],
 })
 export class OpenapiModule {}
