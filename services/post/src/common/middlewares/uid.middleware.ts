13d3852e28fce7cc0803b052f9f08b5082574bc4
lifthus <lifthus531@gmail.com>
2023-05-09 22:14:47 +0900

    [post]uid middleware built

--- services/post/src/common/middlewares/uid.middleware.ts
+++ services/post/src/common/middlewares/uid.middleware.ts
@@ -1,19 +1,24 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request, Response, NextFunction } from 'express';
 import { LifthusSessionJWTPayload } from '../types/session';
 
 @Injectable()
 export class UidMiddleware implements NestMiddleware {
-  constructor(private jwtService: JwtService) {}
-
-  use(req: Request, res: Response, next: NextFunction) {
+  constructor(private readonly jwtService: JwtService) {}
+  async use(req: Request, res: Response, next: NextFunction) {
     if (req.cookies['lifthus_st']) {
       // set uid to req with cookie lifthus_st
       const lstSigned = req.cookies['lifthus_st'];
-      const lst = this.jwtService.verify<LifthusSessionJWTPayload>(lstSigned);
-
-      req.uid = req.cookies['lifthus_st'];
+      try {
+        const lst = await this.jwtService.verifyAsync<LifthusSessionJWTPayload>(
+          lstSigned,
+        );
+        // if lifthus session token is valid, set uid to req
+        req.uid = lst.uid;
+      } catch (e) {
+        // else just nothing
+      }
     }
     next();
   }
