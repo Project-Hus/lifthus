64d7bd5e699236f2c640dfe9120978591337fd53
lifthus <lifthus531@gmail.com>
2023-05-10 23:28:12 +0900

    [post-slug]getSlug func template

--- services/post/src/common/util/util.ts
+++ services/post/src/common/util/util.ts
@@ -4,3 +4,16 @@ export const getCookie = (data: string, ctx: ExecutionContext): string => {
   const request = ctx.switchToHttp().getRequest();
   return request.cookies?.[data] ? request.cookies?.[data] : undefined;
 };
+
+/**
+ * @param {string} data - string to be processed
+ * @returns {string} - slug generated from data
+ * @description
+ * generates slug from string
+ * @example
+ * getSlug('hello world'); // 'hello-world'
+ */
+export const getSlug = (data: string): string => {
+  const slug = data;
+  return slug;
+};
