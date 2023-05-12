4c90a1264b9be5da5c9c6fa11e0d67f8bef56248
lifthus <lifthus531@gmail.com>
2023-05-13 00:01:21 +0900

    [post]working on post apis

--- services/post/src/modules/query/post.query.service.ts
+++ services/post/src/modules/query/post.query.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Post } from '@prisma/client';
 import { Response } from 'express';
+import { PostDBService } from 'src/prisma/post.db.service';
 
 @Injectable()
 export class PostQueryService {
+  constructor(private readonly postDBService: PostDBService) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getUserPosts(): Promise<Post[]> {
+    return this.postDBService.posts({});
   }
 }
--- services/post/src/modules/query/query.controller.ts
+++ services/post/src/modules/query/query.controller.ts
@@ -3,16 +3,24 @@ import { Request, Response } from 'express';
 import { UserGuard } from 'src/common/guards/post.guard';
 import { PostQueryService } from './post.query.service';
 import { CommentQueryService } from './comment.query.service';
+import { Post, Prisma } from '@prisma/client';
+import { PostDBService } from 'src/prisma/post.db.service';
 
 @Controller('/post/query')
 export class QueryController {
   constructor(
     private readonly postQueryService: PostQueryService,
     private readonly commentQueryService: CommentQueryService,
+    private readonly postDBService: PostDBService,
   ) {}
 
   @Get()
   getHello(): string {
     return this.postQueryService.getHello();
+  }
+
+  @Get('/post/:uid')
+  async getPosts(@Req() req: Request, @Res() res: Response): Promise<Post[]> {
+    return this.postDBService.posts({});
   }
 }
