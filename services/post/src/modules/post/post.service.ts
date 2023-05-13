644b2eb730ab576667d041aa29e66e320b104017
lifthus <lifthus531@gmail.com>
2023-05-13 18:07:08 +0900

    [post]composite keys for Like tables

--- services/post/src/modules/post/post.service.ts
+++ services/post/src/modules/post/post.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { Post, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class PostService {
+  constructor(private readonly prisma: PrismaService) {}
   getHello(): string {
     return 'Hello World!';
+  }
+  wirtePost(data: Prisma.PostCreateInput): Promise<Post> {
+    return this.prisma.post.create({
+      data,
+    });
+  }
+
+  updatePost(data: Prisma.PostUpdateInput): Promise<Post> {
+    const pid = Number(data.id);
+    return this.prisma.post.update({
+      data,
+      where: { id: pid },
+    });
+  }
+
+  deletePost(where: Prisma.PostWhereUniqueInput): Promise<Post> {
+    return this.prisma.post.delete({
+      where,
+    });
+  }
+
+  likePost(uid: number, where: Prisma.PostWhereUniqueInput): Promise<Post> {
+    return this.prisma.post.update({
+      data: { likenum: { increment: 1 } },
+      where,
+    });
+  }
+
+  unlikePost(uid: number, where: Prisma.PostWhereUniqueInput): Promise<Post> {
+    return this.prisma.post.update({
+      data: { likenum: { decrement: 1 } },
+      where,
+    });
   }
 }
