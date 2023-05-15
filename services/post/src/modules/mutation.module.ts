9c1f39f6a70c4c2857e38bcc84791cd977156770
lifthus <lifthus531@gmail.com>
2023-05-15 23:27:36 +0900

    #17/feature/comment service dependencies

--- services/post/src/modules/mutation.module.ts
+++ services/post/src/modules/mutation.module.ts
@@ -3,9 +3,10 @@ import { PostController } from './post/post.controller';
 import { CommentController } from './comment/comment.controller';
 import { PostService } from './post/post.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CommentService } from './comment/comment.service';
 
 @Module({
   controllers: [PostController, CommentController],
-  providers: [PostService, PrismaService],
+  providers: [PostService, CommentService, PrismaService],
 })
 export class MutationModule {}
