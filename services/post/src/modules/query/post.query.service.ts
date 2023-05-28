de7f43aea827b6c2af92f818b82721bbc6186dd9
lifthus <lifthus531@gmail.com>
2023-05-28 20:25:19 +0900

    comment orderBy fixed

--- services/post/src/modules/query/post.query.service.ts
+++ services/post/src/modules/query/post.query.service.ts
@@ -56,9 +56,6 @@ export class PostQueryService {
               },
             },
           },
-          orderBy: {
-            createdAt: 'desc',
-          },
         },
         mentions: {
           select: {
