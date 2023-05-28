5a6ad6260d28c9367b7b1ddfb284eac1b72dbf57
lifthus <lifthus531@gmail.com>
2023-05-28 21:06:32 +0900

    #31/feat/relation api init

---
+++ services/lifthus-auth/api/relation/controller.go
@@ -0,0 +1,47 @@
+package user
+
+import (
+	"lifthus-auth/common/guard"
+	"lifthus-auth/ent"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type UserApiControllerParams struct {
+	DbClient   *ent.Client
+	HttpClient *http.Client
+}
+
+// NewUserApiController returns Echo comprising of auth api routes. instance to main.
+func NewUserApiController(userApi *echo.Echo, params UserApiControllerParams) *echo.Echo {
+	userApiController := newUserApiController(params)
+
+	userApi.POST("/auth/user", userApiController.RegisterUser, guard.UserGuard)
+	userApi.PUT("/auth/user", userApiController.SetUserInfo, guard.UserGuard)
+
+	userApi.GET("/auth/user/:uid", userApiController.GetUserInfo)
+	userApi.GET("/auth/username/:username", userApiController.GetUserInfoByUsername)
+
+	return userApi
+}
+
+// newAuthApiController returns a new authApiController that implements every auth api features.
+func newUserApiController(params UserApiControllerParams) userApis {
+	return &userApiController{dbClient: params.DbClient, httpClient: params.HttpClient}
+}
+
+// authApiController defines what auth api has to have and implements authApis interface at service file.
+type userApiController struct {
+	dbClient   *ent.Client
+	httpClient *http.Client
+}
+
+// authApis interface defines what auth api has to handle
+type userApis interface {
+	GetUserInfo(c echo.Context) error
+	GetUserInfoByUsername(c echo.Context) error
+	SetUserInfo(c echo.Context) error
+
+	RegisterUser(c echo.Context) error
+}
---
+++ services/lifthus-auth/api/relation/user.service.go
@@ -0,0 +1,181 @@
+package user
+
+import (
+	"fmt"
+	"lifthus-auth/common/dto"
+	"lifthus-auth/db"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+// GetUserInfo godoc
+// @Router       /user/{uid} [get]
+// @Param uid path string true "user id"
+// @Summary      gets uid from path param and returns user info
+// @Description  if the signed user is the same as the requested user, returns all info while hiding sensitive info if different.
+// @Tags         user
+// @Success      200 "returns user info as json"
+// @Failure      400 "invalid uid"
+// @Failure      404 "user not found"
+// @Failure      500 "failed to create new session"
+func (uc userApiController) GetUserInfo(c echo.Context) error {
+	signedUser, ok := c.Get("uid").(uint64)
+
+	uid, err := strconv.ParseUint(c.Param("uid"), 10, 64)
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	user, err := db.QueryUserByUID(c.Request().Context(), uc.dbClient, uint64(uid))
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+	if user == nil {
+		return c.String(http.StatusNotFound, fmt.Sprintf("user %d not found", uid))
+	}
+
+	var udto *dto.QueryUserDto
+
+	if ok && signedUser == uid {
+		udto = &dto.QueryUserDto{
+			ID:              user.ID,
+			Registered:      user.Registered,
+			RegisteredAt:    user.RegisteredAt,
+			Username:        user.Username,
+			Email:           &user.Email,
+			EmailVerified:   &user.EmailVerified,
+			Name:            &user.Name,
+			GivenName:       &user.GivenName,
+			FamilyName:      &user.FamilyName,
+			Birthdate:       user.Birthdate,
+			ProfileImageURL: *user.ProfileImageURL,
+			CreatedAt:       user.CreatedAt,
+			UpdatedAt:       user.UpdatedAt,
+		}
+	} else {
+		udto = &dto.QueryUserDto{
+			ID:              user.ID,
+			Registered:      user.Registered,
+			RegisteredAt:    user.RegisteredAt,
+			Username:        user.Username,
+			Email:           nil,
+			EmailVerified:   nil,
+			Name:            nil,
+			GivenName:       nil,
+			FamilyName:      nil,
+			Birthdate:       user.Birthdate,
+			ProfileImageURL: *user.ProfileImageURL,
+			CreatedAt:       user.CreatedAt,
+			UpdatedAt:       user.UpdatedAt,
+		}
+	}
+
+	return c.JSON(http.StatusOK, udto)
+}
+
+// GetUserInfoByUsername godoc
+// @Router       /username/{username} [get]
+// @Param username path string true "user id"
+// @Summary      gets username from path param and returns user info
+// @Description  if the signed user is the same as the requested user, returns all info while hiding sensitive info if different.
+// @Tags         user
+// @Success      200 "returns user info as json"
+// @Failure      404 "user not found"
+// @Failure      500 "failed to create new session"
+func (uc userApiController) GetUserInfoByUsername(c echo.Context) error {
+	signedUser, ok := c.Get("uid").(uint64)
+
+	username := c.Param("username")
+
+	user, err := db.QueryUserByUsername(c.Request().Context(), uc.dbClient, username)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+	if user == nil {
+		return c.String(http.StatusNotFound, fmt.Sprintf("user %s not found", username))
+	}
+
+	var udto *dto.QueryUserDto
+
+	if ok && signedUser == user.ID {
+		udto = &dto.QueryUserDto{
+			ID:              user.ID,
+			Registered:      user.Registered,
+			RegisteredAt:    user.RegisteredAt,
+			Username:        user.Username,
+			Email:           &user.Email,
+			EmailVerified:   &user.EmailVerified,
+			Name:            &user.Name,
+			GivenName:       &user.GivenName,
+			FamilyName:      &user.FamilyName,
+			Birthdate:       user.Birthdate,
+			ProfileImageURL: *user.ProfileImageURL,
+			CreatedAt:       user.CreatedAt,
+			UpdatedAt:       user.UpdatedAt,
+		}
+	} else {
+		udto = &dto.QueryUserDto{
+			ID:              user.ID,
+			Registered:      user.Registered,
+			RegisteredAt:    user.RegisteredAt,
+			Username:        user.Username,
+			Email:           nil,
+			EmailVerified:   nil,
+			Name:            nil,
+			GivenName:       nil,
+			FamilyName:      nil,
+			Birthdate:       user.Birthdate,
+			ProfileImageURL: *user.ProfileImageURL,
+			CreatedAt:       user.CreatedAt,
+			UpdatedAt:       user.UpdatedAt,
+		}
+	}
+
+	return c.JSON(http.StatusOK, udto)
+}
+
+// SetUserInfo godoc
+// @Router       /user [put]
+// @Param uid path string true "user id"
+// @Param userinfo body dto.UpdateUserInfoDto true "user info"
+// @Summary      gets uid from path param and updates user info
+// @Description  it gets uid from path param and updates user info
+// @Tags         user
+// @Success      200 "returns user info as json"
+// @Failure      400 "invalid uid"
+// @Failure      404 "user not found"
+// @Failure      500 "failed to create new session"
+func (uc userApiController) SetUserInfo(c echo.Context) error {
+	uid := c.Get("uid").(uint64)
+	userInfo := new(dto.UpdateUserInfoDto)
+	if err := c.Bind(userInfo); err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+	if userInfo.Uid != uid {
+		return c.String(http.StatusUnauthorized, "Unauthorized")
+	}
+	user, err := db.UpdateUserInfo(c.Request().Context(), uc.dbClient, *userInfo)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	qu := &dto.QueryUserDto{
+		ID:              user.ID,
+		Registered:      user.Registered,
+		RegisteredAt:    user.RegisteredAt,
+		Username:        user.Username,
+		Email:           &user.Email,
+		EmailVerified:   &user.EmailVerified,
+		Name:            &user.Name,
+		GivenName:       &user.GivenName,
+		FamilyName:      &user.FamilyName,
+		Birthdate:       user.Birthdate,
+		ProfileImageURL: *user.ProfileImageURL,
+		CreatedAt:       user.CreatedAt,
+		UpdatedAt:       user.UpdatedAt,
+	}
+
+	return c.JSON(http.StatusOK, qu)
+}
