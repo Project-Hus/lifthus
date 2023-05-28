5a41e2b7477f371f28d5b1a812c09adab3e0beba
lifthus <lifthus531@gmail.com>
2023-05-28 21:11:59 +0900

    #31/feat/relation api init

--- services/lifthus-auth/api/relation/controller.go
+++ services/lifthus-auth/api/relation/controller.go
@@ -1,47 +1,39 @@
-package user
+package relation
 
 import (
-	"lifthus-auth/common/guard"
 	"lifthus-auth/ent"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
-type UserApiControllerParams struct {
+type RelationApiControllerParams struct {
 	DbClient   *ent.Client
 	HttpClient *http.Client
 }
 
-// NewUserApiController returns Echo comprising of auth api routes. instance to main.
-func NewUserApiController(userApi *echo.Echo, params UserApiControllerParams) *echo.Echo {
-	userApiController := newUserApiController(params)
+// NewRelationApiController returns Echo comprising of auth api routes. instance to main.
+func NewRelationApiController(relationApi *echo.Echo, params RelationApiControllerParams) *echo.Echo {
+	_ = newRelationApiController(params)
 
-	userApi.POST("/auth/user", userApiController.RegisterUser, guard.UserGuard)
-	userApi.PUT("/auth/user", userApiController.SetUserInfo, guard.UserGuard)
+	relationApi.GET("/auth/relation", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Follower!")
+	})
 
-	userApi.GET("/auth/user/:uid", userApiController.GetUserInfo)
-	userApi.GET("/auth/username/:username", userApiController.GetUserInfoByUsername)
-
-	return userApi
+	return relationApi
 }
 
 // newAuthApiController returns a new authApiController that implements every auth api features.
-func newUserApiController(params UserApiControllerParams) userApis {
-	return &userApiController{dbClient: params.DbClient, httpClient: params.HttpClient}
+func newRelationApiController(params RelationApiControllerParams) relationApis {
+	return &relationApiController{dbClient: params.DbClient, httpClient: params.HttpClient}
 }
 
 // authApiController defines what auth api has to have and implements authApis interface at service file.
-type userApiController struct {
+type relationApiController struct {
 	dbClient   *ent.Client
 	httpClient *http.Client
 }
 
 // authApis interface defines what auth api has to handle
-type userApis interface {
-	GetUserInfo(c echo.Context) error
-	GetUserInfoByUsername(c echo.Context) error
-	SetUserInfo(c echo.Context) error
-
-	RegisterUser(c echo.Context) error
+type relationApis interface {
 }
