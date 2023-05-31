6fb230d8d49d2c29793431c1c25560f1faebf0df
lifthus <lifthus531@gmail.com>
2023-05-31 20:13:07 +0900

    #22/feat/routine ValidateLST

--- services/routine/common/helper/lst.go
+++ services/routine/common/helper/lst.go
@@ -1,9 +1,28 @@
 package helper
 
-import "lifthus-auth/common/helper"
+import (
+	"fmt"
+	"lifthus-auth/common/helper"
+	"strconv"
+)
 
 func ValidateLST(lst string) (uid *uint64, err error) {
 	lstClaims, exp, err := helper.ParseJWTwithHMAC(lst)
+	if err != nil {
+		return nil, err
+	}
+	if exp {
+		return nil, nil
+	}
+	purpose, ok := lstClaims["purpose"].(string)
+	if !ok || purpose != "lifthus_session" {
+		return nil, fmt.Errorf("invalid token")
+	}
+	uidString, ok := lstClaims["uid"].(string)
+	if !ok {
+		return nil, nil
+	}
+	*uid, err = strconv.ParseUint(uidString, 10, 64)
 	return uid, err
 }
 
