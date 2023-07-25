b4514571793b704a71e98a6c27fb65048a7406b8
lifthus <lifthus531@gmail.com>
2023-07-25 20:32:31 +0900

    #63/imp/old apis removed

--- services/lifthus-auth/common/service/session/session.go
+++ services/lifthus-auth/common/service/session/session.go
@@ -36,9 +36,9 @@ func IsExpiredValid(err error) bool {
 	return err == ExpiredValidSessionError
 }
 
-// ValidateSessionV2 only validates the session token.
+// ValidateSession only validates the session token.
 // and returns the session ID, user ID, whether it is expired, and error.
-func ValidateSessionV2(ctx context.Context, lst string) (
+func ValidateSession(ctx context.Context, lst string) (
 	sid *uuid.UUID,
 	uid *uint64,
 	exp bool,
@@ -120,7 +120,7 @@ func ValidateSessionQueryUser(ctx context.Context, lst string) (ls *ent.Session,
 }
 
 // CreateSession issues new Lifthus session and returns the session entity and signed session token.
-func CreateSessionV2(ctx context.Context) (ls *ent.Session, newSignedToken string, err error) {
+func CreateSession(ctx context.Context) (ls *ent.Session, newSignedToken string, err error) {
 	// create new lifthus session
 	ns, err := db.Client.Session.Create().Save(ctx)
 	if err != nil {
@@ -278,150 +278,4 @@ func RefreshSessionHard(ctx context.Context, ls *ent.Session) (nls *ent.Session,
 	}
 
 	return nls, stSigned, nil
-}
-
-// ========================================================================================
-
-// CreateLifthusSession creates new session for user and returns the session ID.
-func CreateSession(ctx context.Context, client *ent.Client) (sid string, stSigned string, err error) {
-	// create new lifthus session
-	ns, err := client.Session.Create().Save(ctx)
-	if err != nil {
-		return "", "", fmt.Errorf("!!creating new session failed:%w", err)
-	}
-
-	// create new jwt session token with session id
-	st := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"purpose": "lifthus_session",
-		"sid":     ns.ID.String(),
-		"exp":     time.Now().Add(time.Minute * 5).Unix(),
-	})
-
-	// sign and get the complete encoded token as a string using the secret
-	hsk := []byte(lifthus.HusSecretKey)
-	stSigned, err = st.SignedString(hsk)
-	if err != nil {
-		return "", "", fmt.Errorf("!!signing session token failed:%w", err)
-	}
-
-	return ns.ID.String(), stSigned, nil
-}
-
-func RevokeSession(ctx context.Context, client *ent.Client, sid string) error {
-	sid_uuid, err := uuid.Parse(sid)
-	if err != nil {
-		return fmt.Errorf("!!parsing uuid failed:%w", err)
-	}
-	// delete the session from database
-	err = client.Session.DeleteOneID(sid_uuid).Exec(ctx)
-	if err != nil && !ent.IsNotFound(err) {
-		return fmt.Errorf("!!revoking session failed:%w", err)
-	}
-	return nil
-}
-
-func SignSession(ctx context.Context, client *ent.Client, sid string, uid uint64) error {
-	sid_uuid, err := uuid.Parse(sid)
-	if err != nil {
-		return fmt.Errorf("!!parsing uuid failed: %w", err)
-	}
-	// update the session with uid
-	_, err = client.Session.UpdateOneID(sid_uuid).SetUID(uid). /*SetUsed(false).*/ Save(ctx)
-	if err != nil {
-		return fmt.Errorf("!!updating session with uid failed: %w", err)
-	}
-	return nil
-}
-
-// UUID version
-// func SignSession(ctx context.Context, client *ent.Client, sid string, uid string) error {
-// 	sid_uuid, err := uuid.Parse(sid)
-// 	uid_uuid, err1 := uuid.Parse(uid)
-// 	if err != nil || err1 != nil {
-// 		return fmt.Errorf("!!parsing uuid failed: %w, %w", err, err1)
-// 	}
-// 	// update the session with uid
-// 	_, err = client.Session.UpdateOneID(sid_uuid).SetUID(uid_uuid).SetUsed(false).Save(ctx)
-// 	if err != nil {
-// 		return fmt.Errorf("!!updating session with uid failed: %w", err)
-// 	}
-// 	return nil
-// }
-
-// ValidateSession validates the session and updates the db.
-// cases:
-// B-1: if it is signed but expired, reset used, signed_at, uid from db and return same SID and empty UID
-// B-2: if it is not signed and expired, return same SID
-// C: if it is valid, just return
-func ValidateSession(ctx context.Context, client *ent.Client, st string) (
-	sid string,
-	uid string,
-	exp bool,
-	err error,
-) {
-	// parse session token
-	stParsed, exp, err := helper.ParseJWTWithHMAC(st)
-	if err != nil {
-		return "", "", false, fmt.Errorf("parsing jwt token failed:%w", err)
-	}
-	pps, ok := stParsed["purpose"].(string)
-	if !ok || pps != "lifthus_session" {
-		return "", "", false, fmt.Errorf("parsing jwt token failed: wrong purpose")
-	}
-	// get sid and uid, if not found, return error
-	sid, ok = stParsed["sid"].(string)
-	if !ok {
-		return "", "", false, fmt.Errorf("parsing jwt token failed: sid not found")
-	}
-	uid, ok = stParsed["uid"].(string)
-	if !ok {
-		return "", "", false, fmt.Errorf("parsing jwt token failed: uid not found")
-	}
-	sid_uuid, err := uuid.Parse(sid)
-	if err != nil {
-		return "", "", false, fmt.Errorf("parsing uuid failed:%w", err)
-	}
-
-	// if it is expired and signed, update the session in db to be unsigned.
-	if exp && uid != "" {
-		err = client.Session.UpdateOneID(sid_uuid). /*SetUsed(false).*/ ClearSignedAt().ClearUID().Exec(ctx)
-		if err != nil {
-			return "", "", false, fmt.Errorf("updating session failed:%w", err)
-		}
-	}
-	return sid, uid, exp, nil
-}
-
-// RefreshSession refreshes the session token with same SID and empty UID.
-func RefreshSessionToken(ctx context.Context, client *ent.Client, sid string) (stSigned string, err error) {
-	// create new jwt session token with session id
-	st := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"purpose": "lifthus_session",
-		"sid":     sid,
-		"exp":     time.Now().Add(time.Minute * 5).Unix(),
-	})
-	stSigned, err = st.SignedString([]byte(lifthus.HusSecretKey))
-	if err != nil {
-		return "", err
-	}
-	return stSigned, nil
-}
-
-// RevokeHusToken takes session token and revokes them.
-func RevokeSessionToken(ctx context.Context, client *ent.Client, st string) error {
-	stClaims, _, err := helper.ParseJWTWithHMAC(st)
-	if err != nil {
-		return err
-	}
-
-	sid_uuid, err := uuid.Parse(stClaims["sid"].(string))
-	if err != nil {
-		return err
-	}
-
-	err = client.Session.DeleteOneID(sid_uuid).Exec(ctx)
-	if err != nil && !ent.IsNotFound(err) {
-		return err
-	}
-	return nil
 }
