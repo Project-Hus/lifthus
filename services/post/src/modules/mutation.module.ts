2f054978ce4f9a3f7bb87bfcf6f6da76adb3be15
lifthus <lifthus531@gmail.com>
2023-07-30 21:43:52 +0900

    #77/imp/post/exception filters for each module

--- services/post/src/modules/mutation.module.ts
+++ services/post/src/modules/mutation.module.ts
@@ -4,9 +4,16 @@ import { CommentController } from './comment/comment.controller';
 import { PostService } from './post/post.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CommentService } from './comment/comment.service';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
 
 @Module({
   controllers: [PostController, CommentController],
-  providers: [PostService, CommentService, PrismaService],
+  providers: [
+    PostService,
+    CommentService,
+    PrismaService,
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+  ],
 })
 export class MutationModule {}
