1c4d8b7ae60b113ac32b4b8f31b5d1208973daae
lifthus <lifthus531@gmail.com>
2023-08-07 19:36:40 +0900

    #78/feat/microservices fixed

--- services/post/src/common/middlewares/uid.middleware.ts
+++ services/post/src/common/middlewares/uid.middleware.ts
@@ -14,51 +14,6 @@ import { Request, Response, NextFunction } from 'express';
 export class UidMiddleware implements NestMiddleware {
   constructor(private readonly jwtService: JwtService) {}
   async use(req: Request, res: Response, next: NextFunction) {
-    // get the origin of request
-    const origin = req.headers['origin'];
-    if (origin === 'http://localhost:3000') {
-      const authHeader = req.headers['authorization'];
-      if (
-        authHeader &&
-        authHeader.startsWith('Bearer ') &&
-        authHeader.length > 7
-      ) {
-        const lstSigned = authHeader.slice(7);
-        try {
-          const lst =
-            await this.jwtService.verifyAsync<LifthusSessionJWTPayload>(
-              lstSigned,
-            );
-          if (lst.uid) {
-            req.uid = parseInt(lst.uid);
-          }
-        } catch (e) {}
-      }
-    } else {
-      if (req.cookies['lifthus_st']) {
-        // set uid to req with cookie lifthus_st
-        const lstSigned = req.cookies['lifthus_st'];
-        try {
-          const lst =
-            await this.jwtService.verifyAsync<LifthusSessionJWTPayload>(
-              lstSigned,
-            );
-          req.exp = false;
-          // if lifthus session token is valid, set uid to req
-          // lst.uid to number
-          if (lst.uid) {
-            req.uid = parseInt(lst.uid);
-          }
-        } catch (e: any) {
-          if (e.name === 'TokenExpiredError') {
-            req.exp = true;
-          }
-        }
-      }
-    }
-    next();
-  }
-  async useV2(req: Request, res: Response, next: NextFunction) {
     // get Authorization header
     const authHeader = req.headers['authorization'];
     if (authHeader && authHeader.startsWith('Bearer ')) {
