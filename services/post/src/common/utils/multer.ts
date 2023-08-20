8d640533026faadc09f3360fd79cd299612cbe0b
lifthus <lifthus531@gmail.com>
2023-08-21 00:09:31 +0900

    #103/feat/template env vars and multer modified

--- services/post/src/common/utils/multer.ts
+++ services/post/src/common/utils/multer.ts
@@ -3,16 +3,15 @@ import multer from 'multer';
 import multerS3 from 'multer-s3';
 import crypto from 'crypto';
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'native') {
   aws.config.loadFromPath('../../../config/s3.json');
-} else if (process.env.NODE_ENV === 'development') {
+} else {
   aws.config.update({
     accessKeyId: process.env.LIFTHUS_ACCESS_KEY_ID,
     secretAccessKey: process.env.LIFTHUS_SECRET_ACCESS_KEY,
     region: 'us-west-2',
   });
 }
-
 const s3 = new aws.S3();
 const upload = multer({
   storage: multerS3({
