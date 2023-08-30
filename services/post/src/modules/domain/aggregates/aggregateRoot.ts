c7f99f02e7b55743b5831ca9dcac8c29ddd23d7b
lifthus <lifthus531@gmail.com>
2023-08-30 23:16:56 +0900

    #110/imp/AggregateRoot

---
+++ services/post/src/modules/domain/aggregates/aggregateRoot.ts
@@ -0,0 +1,25 @@
+export class AggregateRoot {
+  constructor(
+    private created: boolean = false,
+    private updated: boolean = false,
+    private deleted: boolean = false,
+  ) {}
+  private setCreated() {
+    this.created = true;
+  }
+  private setUpdated() {
+    this.updated = true;
+  }
+  private setDeleted() {
+    this.deleted = true;
+  }
+  isCreated() {
+    return this.created;
+  }
+  isUpdated() {
+    return this.updated;
+  }
+  isDeleted() {
+    return this.deleted;
+  }
+}
