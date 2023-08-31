e880c42db9d7962fa2782954d5024746d3068112
lifthus <lifthus531@gmail.com>
2023-08-31 19:58:34 +0900

    #110/imp/AbstractPostRepository simplified

--- services/post/src/modules/domain/repositories/post.repository.ts
+++ services/post/src/modules/domain/repositories/post.repository.ts
@@ -1,27 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { Post } from '../aggregates/post/post.model';
 import { User } from '../aggregates/user/user.model';
-
 import { PostSummary } from '../aggregates/post/postSummary.model';
-import { stringifyAny } from 'src/common/utils/utils';
 
 @Injectable()
 export abstract class PostRepository {
-  private posts: Map<bigint, Post> = new Map();
-  private postOrigins: Map<bigint, string> = new Map();
-
-  private getCachekey(p: Post): bigint {
-    return BigInt(p.getID());
-  }
-
-  private getCacheString(p: Post): string {
-    return stringifyAny(p);
-  }
-
-  private isUpdated(p: Post, origin: string): boolean {
-    return this.getCacheString(p) !== origin;
-  }
-
   async getAllPostSumms(skip: number): Promise<PostSummary[]> {
     return await this._getAllPostSumms(skip);
   }
@@ -31,55 +14,23 @@ export abstract class PostRepository {
   }
 
   async getPostByID(pid: bigint): Promise<Post | null> {
-    const post = await this._getPostByID(pid);
-    if (!post) return null;
-    const cacheKey = this.getCachekey(post);
-    this.posts.set(cacheKey, post);
-    this.postOrigins.set(cacheKey, this.getCacheString(post));
-    return post;
+    return await this._getPostByID(pid);
   }
 
   async getPostBySlug(slug: string): Promise<Post | null> {
-    const post = await this._getPostBySlug(slug);
-    if (!post) return null;
-    const cacheKey = this.getCachekey(post);
-    this.posts.set(cacheKey, post);
-    this.postOrigins.set(cacheKey, this.getCacheString(post));
-    return post;
+    return await this._getPostBySlug(slug);
   }
 
   async createPost(post: Post): Promise<Post> {
-    const newPost = await this._createPost(post);
-    const cacheKey = this.getCachekey(newPost);
-    this.posts.set(cacheKey, newPost);
-    return newPost;
+    return await this._createPost(post);
   }
 
   async deletePost(post: Post): Promise<Post> {
-    const cacheKey = this.getCachekey(post);
-    this.posts.delete(cacheKey);
-    this.postOrigins.delete(cacheKey);
     return await this._deletePost(post);
   }
 
-  private clear() {
-    this.posts.clear();
-    this.postOrigins.clear();
-  }
-
-  async save(): Promise<void> {
-    const changes: Set<Post> = new Set();
-    this.posts.forEach((post, key) => {
-      const origin = this.postOrigins.get(key);
-      if (this.isUpdated(post, origin)) changes.add(post);
-    });
-    this.clear();
-    return this._save(changes);
-  }
-
-  async cancel(): Promise<void> {
-    this.clear();
-    return this._save(new Set());
+  async save(post: Post): Promise<Post> {
+    return this._save(post);
   }
 
   // Abstract methods to be implemented by the actual repository
@@ -96,5 +47,5 @@ export abstract class PostRepository {
   abstract _createPost(post: Post): Promise<Post>;
   abstract _deletePost(traget: Post): Promise<Post>;
 
-  abstract _save(changes: Set<Post>): Promise<void>;
+  abstract _save(post: Post): Promise<Post>;
 }
