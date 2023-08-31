7199d8fe6f02f2bf15d248355208a2f7455b8840
lifthus <lifthus531@gmail.com>
2023-08-31 12:29:27 +0900

    #110/imp/UserRepo not abstract

--- services/post/src/modules/domain/repositories/user.repository.ts
+++ services/post/src/modules/domain/repositories/user.repository.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { User } from '../aggregates/user/user.model';
 
 @Injectable()
-export abstract class UserRepository {
+export class UserRepository {
   constructor() {}
   getUser(id: bigint): User {
     return new User(id);
