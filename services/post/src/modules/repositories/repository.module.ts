bca50e3930def776659efc438b2e2ed59ac57c28
lifthus <lifthus531@gmail.com>
2023-08-31 14:28:23 +0900

    #110/imp/misc

--- services/post/src/modules/repositories/repository.module.ts
+++ services/post/src/modules/repositories/repository.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { DomainModule } from '../domain/domain.module';
 import { PostRepository } from '../domain/repositories/post.repository';
 import { PrismaPostRepository } from './post.repository';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   imports: [DomainModule],
   providers: [
+    PrismaService,
     {
       provide: PostRepository,
       useClass: PrismaPostRepository,
