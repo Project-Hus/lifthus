db7bb5bc3ca69f917741e91e626d18471c7c97df
lifthus <lifthus531@gmail.com>
2023-08-31 14:30:59 +0900

    #110/imp/misc

--- services/post/src/modules/repositories/user.repository.ts
+++ services/post/src/modules/repositories/user.repository.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '../domain/aggregates/user/user.model';
+import { UserRepository as AbstractUserRepository } from '../domain/repositories/user.repository';
 
 @Injectable()
-export class UserRepository {
-  constructor() {}
+export class UserRepository extends AbstractUserRepository {
   getUser(id: bigint): User {
     return new User(id);
   }
