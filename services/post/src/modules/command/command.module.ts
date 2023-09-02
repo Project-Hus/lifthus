d5e2c0e4025676f0a8bc27d30ff21aaf59473354
lifthus <lifthus531@gmail.com>
2023-09-02 23:51:45 +0900

    #110/imp/abs paths

--- services/post/src/modules/command/command.module.ts
+++ services/post/src/modules/command/command.module.ts
@@ -1,9 +1,9 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
-import { PostController } from './post/post.controller';
-import { CommentController } from './comment/comment.controller';
-import { PostService } from './post/post.service';
+import { PostController } from 'src/modules/command/post/post.controller';
+import { CommentController } from 'src/modules/command/comment/comment.controller';
+import { PostService } from 'src/modules/command/post/post.service';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CommentService } from './comment/comment.service';
+import { CommentService } from 'src/modules/command/comment/comment.service';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
 
