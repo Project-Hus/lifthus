df34b16808d86ec56be8173a75e50985e320b79a
lifthus <lifthus531@gmail.com>
2023-09-04 15:52:22 +0900

    #110/imp/command modules separated

--- services/post/src/modules/command/command.module.ts
+++ services/post/src/modules/command/command.module.ts
@@ -1,29 +1,14 @@
 import { Module } from '@nestjs/common';
 
-import { PostController } from 'src/modules/command/post/post.controller';
-import { CommentController } from 'src/modules/command/comment/comment.controller';
-
-import { PostService } from 'src/modules/command/post/post.service';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { CommentService } from 'src/modules/command/comment/comment.service';
-import { S3Service } from 'src/modules/command/post/s3.service';
-
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
 
-import { RepositoryModule } from 'src/modules/repositories/repository.module';
-import { Post2Service } from 'src/modules/command/post/post2.service';
+import { PostCommandModule } from 'src/modules/command/post/post.command.module';
+import { CommentCommandModule } from 'src/modules/command/comment/comment.command.module';
 
 @Module({
-  imports: [RepositoryModule],
-  controllers: [PostController, CommentController],
-  providers: [
-    PostService,
-    Post2Service,
-    CommentService,
-    PrismaService,
-    S3Service,
-    { provide: APP_FILTER, useClass: HttpExceptionFilter },
-  ],
+  imports: [PostCommandModule, CommentCommandModule],
+  controllers: [],
+  providers: [{ provide: APP_FILTER, useClass: HttpExceptionFilter }],
 })
 export class CommandModule {}
---
+++ services/post/src/modules/command/comment/comment.command.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+
+import { CommentController } from 'src/modules/command/comment/comment.controller';
+
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CommentService } from 'src/modules/command/comment/comment.service';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
+
+import { RepositoryModule } from 'src/modules/repositories/repository.module';
+
+@Module({
+  imports: [RepositoryModule],
+  controllers: [CommentController],
+  providers: [
+    CommentService,
+    PrismaService,
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+  ],
+})
+export class CommentCommandModule {}
