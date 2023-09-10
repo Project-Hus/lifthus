d119508c4548bf2936403d4d3341457fe39bd6f2
lifthus <lifthus531@gmail.com>
2023-09-10 19:21:34 +0900

    #122/imp/dto detacheded from models

--- services/post/src/domain/aggregates/post/postSummary.model.ts
+++ services/post/src/domain/aggregates/post/postSummary.model.ts
@@ -1,17 +1,5 @@
-import { PostIds } from 'src/domain/aggregates/post/post.vo';
-import { Timestamps } from 'src/domain/vo';
-
-export type PostSumm = {
-  id: bigint;
-  author: bigint;
-  createdAt: Date;
-  updatedAt: Date;
-  imageSrcs: string[];
-  slug: string;
-};
-
 export class PostSummary {
-  private constructor(
+  constructor(
     public readonly author: bigint,
     public readonly id: bigint,
     public readonly slug: string,
@@ -19,20 +7,4 @@ export class PostSummary {
     public readonly createdAt: Date,
     public readonly updatedAt: Date,
   ) {}
-
-  static create(
-    author: bigint,
-    posdIds: PostIds,
-    imageSrcs: string[],
-    timestamps: Timestamps,
-  ): PostSummary {
-    return new PostSummary(
-      author,
-      posdIds.id,
-      posdIds.slug,
-      [...imageSrcs],
-      timestamps.createdAt,
-      timestamps.updatedAt,
-    );
-  }
 }
