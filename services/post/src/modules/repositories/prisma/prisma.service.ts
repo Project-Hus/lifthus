372216de9552bec5ca2bdbb5acd58487696012c0
lifthus <lifthus531@gmail.com>
2023-09-13 23:14:26 +0900

    dev/PrismaService disconnects beforeExit

--- services/post/src/modules/repositories/prisma/prisma.service.ts
+++ services/post/src/modules/repositories/prisma/prisma.service.ts
@@ -5,12 +5,8 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
-  }
-
-  async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
       await this.$disconnect();
-      await app.close();
     });
   }
 }
