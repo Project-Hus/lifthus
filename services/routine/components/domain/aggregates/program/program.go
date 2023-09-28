9239f72120aeeae724f4235603110f61339ff1b0
lifthus <lifthus531@gmail.com>
2023-09-28 23:31:33 +0900

    #131/feat/basic Program

--- services/routine/components/domain/aggregates/program/program.go
+++ services/routine/components/domain/aggregates/program/program.go
@@ -5,99 +5,20 @@ import (
 	"routine/components/domain/aggregates/user"
 )
 
-type ProgramId domain.Id
 type ProgramCode domain.Code
 
 type ProgramTitle string
 
 type Program struct {
-	id   *ProgramId
 	code ProgramCode
 
-	author      user.UserId
 	programType ProgramType
-	createdAt   domain.CreatedAt
-	updatedAt   *domain.UpdatedAt
+	title       ProgramTitle
+	author      user.UserId
 
-	derivedFrom *ProgramId
-	deriving    []ProgramId
+	createdAt domain.CreatedAt
 
-	title     ProgramTitle
-	imageSrcs ProgramImageSrcs
-	text      ProgramText
+	derivedFrom *ProgramVersionCode
 
-	versions []*Version
-}
-
-func (p *Program) Update(updater *user.User, updates ProgramUpdates) (*Program, error) {
-	if p.author != updater.Id() {
-		return nil, domain.ErrUnauthorized
-	}
-	switch {
-	case updates.Title != nil:
-		p.title = *updates.Title
-	case updates.ImageSrcs != nil:
-		p.imageSrcs = *updates.ImageSrcs
-	case updates.Text != nil:
-		p.text = *updates.Text
-	}
-	return p, nil
-}
-
-func (p *Program) GetUpdateTargets() ProgramUpdateTargets {
-	return ProgramUpdateTargets{
-		Title:     p.title,
-		ImageSrcs: p.imageSrcs,
-		Text:      p.text,
-	}
-}
-
-func (p *Program) Delete(deleter *user.User) (*Program, error) {
-	if p.author != deleter.Id() {
-		return nil, domain.ErrUnauthorized
-	}
-	if len(p.deriving) > 0 {
-		return nil, ErrExistingDerivingProgram
-	}
-	return p, nil
-}
-
-func (p Program) IsPersisted() bool {
-	return p.id != nil
-}
-
-func (p Program) Id() ProgramId {
-	return *p.id
-}
-
-func (p Program) Code() ProgramCode {
-	return p.code
-}
-
-func (p Program) Metadata() programMetadata {
-	return programMetadata{
-		Title:       p.title,
-		Author:      p.author,
-		ProgramType: p.programType,
-		CreatedAt:   p.createdAt,
-		UpdatedAt:   p.updatedAt,
-	}
-}
-
-func (p Program) Derivations() programDerivations {
-	return programDerivations{
-		DerivedFrom: p.derivedFrom,
-		Deriving:    p.deriving,
-	}
-}
-
-func (p Program) Description() programDescription {
-	return programDescription{
-		ImageSrcs: p.imageSrcs,
-		Text:      p.text,
-	}
-}
-
-func (p Program) Versions() []*Version {
-	return p.versions
+	versions []*ProgramVersion
 }
