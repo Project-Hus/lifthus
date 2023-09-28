95bf7fe868b1804e5b9b287cbebccd7f3f2dc741
lifthus <lifthus531@gmail.com>
2023-09-28 23:34:17 +0900

    #131/feat/working on program

--- services/routine/components/domain/aggregates/program/program_validation.go
+++ services/routine/components/domain/aggregates/program/program_validation.go
@@ -3,25 +3,6 @@ package program
 import "routine/components/domain"
 
 const (
-	TITLE_MAX_LENGTH               = domain.PROGRAM_TITLE_MAX_LENGTH
-	IMAGES_MAX_COUNT               = domain.PROGRAM_IMAGES_MAX_COUNT
-	PROGRAM_DESCRIPTION_MAX_LENGTH = domain.PROGRAM_DESCRIPTION_MAX_LENGTH
+	TITLE_MIN_LENGTH = domain.PROGRAM_TITLE_MIN_LENGTH
+	TITLE_MAX_LENGTH = domain.PROGRAM_TITLE_MAX_LENGTH
 )
-
-func IsProgramInfoValid(info programInfo) bool {
-	return IsTitleValid(info.Title) &&
-		IsImageSrcsValid(info.ImageSrcs) &&
-		IsDescriptionValid(info.Description)
-}
-
-func IsTitleValid(title ProgramTitle) bool {
-	return len(title) <= TITLE_MAX_LENGTH
-}
-
-func IsImageSrcsValid(imageSrcs ProgramImageSrcs) bool {
-	return len(imageSrcs) <= IMAGES_MAX_COUNT
-}
-
-func IsDescriptionValid(description ProgramDescription) bool {
-	return len(description) <= PROGRAM_DESCRIPTION_MAX_LENGTH
-}
