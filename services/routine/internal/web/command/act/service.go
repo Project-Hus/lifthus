cdcb03d519333d80489b9374da343d732807661b
lifthus <lifthus531@gmail.com>
2023-10-02 15:27:50 +0900

    #138/feat/routine/basic createAct service

--- services/routine/internal/web/command/act/service.go
+++ services/routine/internal/web/command/act/service.go
@@ -1,11 +1,26 @@
 package act
 
-import "routine/internal/web/dto"
+import (
+	"fmt"
+	"routine/internal/domain/aggregates/act"
+	"routine/internal/domain/aggregates/user"
+	"routine/internal/web/dto"
+)
 
 type actService struct {
 }
 
 func (as *actService) createAct(dto dto.CreateActDto) (*dto.QueryActDto, error) {
+	actType, err := act.MapActType(dto.ActType)
+	if err != nil {
+		return nil, err
+	}
+	author := user.UserFrom(user.UserId(dto.Author))
+	newAct, err := act.CreateAct(*actType, act.ActName(dto.Name), *author, dto.ImageSrcs, act.ActText(dto.Text))
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(newAct)
 	return nil, nil
 }
 
